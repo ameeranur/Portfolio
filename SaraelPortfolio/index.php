b9786ed2366447d04106f282588861981b147af1
ameeranur <159556022+ameeranur@users.noreply.github.com>
2024-02-17 01:09:53 +0800

    Add files via upload
    Progress Week 1

---
+++ SaraelPortfolio/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Ameera's Portfolio</title>
+    <link rel="stylesheet" href="css/homestyle.css">
+</head>
+<body>
+
+    <div class="hero">
+        <nav>
+            <img src="images/logo.png" alt="logo" class="logo" width="167" height="69">
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="intro.php">ABOUT</a></li>
+                <li><a href="#">PORTFOLIO</a></li>
+                <li><a href="#">PROJECTS</a></li>
+                <li><a href="contact.php">HIRE ME </a></li>                
+            </ul>
+        </nav>
+        <div class="detail">
+            <h1>I'm Ameera <span>Sarael</span></h1>
+            <p>Welcome to my official portfolio.
+                <br>ON THE PROCESS OF MAKING
+            </p>
+        <a href="Ameera Sarael_CV.pdf" target="_blank">Download CV</a>
+        <a href="contact.php">Contact Me</a>
+        </div>
+
+        <div class="images">
+            <img src="images/shape.png" class="shape">
+            <img src="images/ameera.png" class="girl">
+        </div>
+
+    </div>
+
+</body>
+</html>
---
+++ srlportfolio.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 16, 2024 at 06:02 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `srlportfolio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients_tbl`
+--
+
+CREATE TABLE `clients_tbl` (
+  `id` int(11) NOT NULL,
+  `fullName` varchar(50) NOT NULL,
+  `mobileNo` varchar(12) NOT NULL,
+  `emailId` varchar(50) NOT NULL,
+  `message` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `clients_tbl`
+--
+ALTER TABLE `clients_tbl`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `clients_tbl`
+--
+ALTER TABLE `clients_tbl`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
